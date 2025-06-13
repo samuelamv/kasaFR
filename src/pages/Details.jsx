b9aa4e74fb730a0ba2cb4ba9a80838edf812fd6e
Samuel Martins Vieira <samuelalexandre.nov@gmail.com>
2025-06-13 14:53:58 +0200

    modif component accordeons pour la page bout + css

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,23 +1,46 @@
-import { useParams, Navigate } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 import Locations from '../data/Locations.json'
 import Slider from '../components/Slider'
 import InfoLocation from '../components/InfoLocation'
 import TagAndRating from '../components/Tag'
-import Description from '../components/Description'
+import Dropdown from '../components/Dropdown'
 
 function Details() {
   const { id } = useParams()
-  const location = Locations.find((item) => item.id === id)
+  const navigate = useNavigate()
+  const [logement, setLogement] = useState(null)
 
-  if (!location) {
-    return <Navigate to="/error" replace />
-  }
+  useEffect(() => {
+    const found = Locations.find(item => item.id === id)
+    if (found) {
+      setLogement(found)
+    } else {
+      navigate('/404') // redirige si id invalide
+    }
+  }, [id, navigate])
+
+  if (!logement) return null // ou un spinner
   return (
     <div className="details">
       <Slider />
       <InfoLocation />
       <TagAndRating />
-      <Description />
+      <div className="accordion_container">
+      <Dropdown title="Description">
+        <h2 className='accordionTitle'>Description</h2>
+        <p>{logement.description}</p>
+      </Dropdown>
+
+      <Dropdown title="Équipements">
+        <h2 className='accordionTitle'>Equipement</h2>
+        <ul>
+          {logement.equipments.map((equipement, index) => (
+            <li key={index}>{equipement}</li>
+          ))}
+        </ul>
+      </Dropdown>
+    </div>
     </div>
   )
 }
