f5d324c5e64447e507a03d54b4f0c6b565d71d87
Samuel Martins Vieira <samuelalexandre.nov@gmail.com>
2025-06-27 10:55:21 +0200

    structure composant pour la page détail modifié + css + responsive

--- src/components/InfoLocation.jsx
+++ src/components/InfoLocation.jsx
@@ -1,5 +1,5 @@
 // components/InfoLocation.jsx
-import Locations from '../data/Locations.json'
+/*import Locations from '../data/Locations.json'
 import { useParams } from 'react-router-dom'
 import '../styles/InfoLocation.scss'
 
@@ -24,6 +24,30 @@ const InfoLocation = () => {
     </div>
 
     )
+}
+
+export default InfoLocation*/
+import Locations from '../data/Locations.json'
+import { useParams } from 'react-router-dom'
+import '../styles/InfoLocation.scss'
+
+const InfoLocation = () => {
+  const { id } = useParams()
+  const logement = Locations.find((item) => item.id === id)
+
+  return (
+    <div className="info-location">
+      <div className="info-location_left">
+        <h1 className="info-location_title">{logement.title}</h1>
+        <p className="info-location_location">{logement.location}</p>
+        <div className="info-location_tags">
+          {logement.tags.map((tag, index) => (
+            <span className="info-location_tag" key={index}>{tag}</span>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default InfoLocation
--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom'
+/*import { useParams } from 'react-router-dom'
 import Locations from '../data/Locations.json'
 import '../styles/Tag.scss'
 
@@ -33,4 +33,42 @@ const TagAndRating = () => {
   )
 }
 
-export default TagAndRating
+export default TagAndRating*/
+import { useParams } from 'react-router-dom'
+import Locations from '../data/Locations.json'
+import '../styles/Tag.scss'
+
+const HostAndRating = () => {
+  const { id } = useParams()
+  const logement = Locations.find((item) => item.id === id)
+
+  if (!logement) {
+    return <p>Logement introuvable.</p>
+  }
+
+  const fullStars = parseInt(logement.rating)
+  const maxStars = 5
+
+  return (
+    <div className="host-rating">
+      <div className="host-stars">
+        {[...Array(maxStars)].map((_, i) => (
+          <i
+            key={i}
+            className={`fa-star ${i < fullStars ? 'fas' : 'far'}`}
+          ></i>
+        ))}
+      </div>
+      <div className="host-info">
+        <p className="host-name">{logement.host.name}</p>
+        <img
+          src={logement.host.picture}
+          alt={logement.host.name}
+          className="host-picture"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default HostAndRating
